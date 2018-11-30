3dad6cab0a8ee459ee7320501ad583dd64150177
Dan Klasson <dan.g.klasson@gmail.com>
2018-11-30 20:26:56 +0700

    initialized boilerplate

---
+++ src/twitter.js
@@ -0,0 +1,7 @@
+// normally we'd use env variables here
+var client = new Twitter({
+  consumer_key: "9UEJhJe5F3F4WRD5xHxLAToab",
+  consumer_secret: "DM0GyMhCBy0WlzexGGwrCxOL2ZDppIFBYXz4I8FXvKuQ2zjOmX",
+  access_token_key: "1068356140927377409-PhgULPZsTiKJye1GcZJXEPUP5V1TDi",
+  access_token_secret: "GJ741fgVhFFpzlveGlP8uqabnmAvRrGvnEZvGEPekHIHi"
+});
