371a099073f7558cceacb19e23e5580fd40d128f
Dan Klasson <dan.g.klasson@gmail.com>
2018-11-30 20:27:30 +0700

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+
+const Index = () => <h2>Tweets</h2>;
+const Settings = () => <h2>Settings</h2>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/settings/">Settings</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Index} />
+          <Route path="/settings/" component={Settings} />
+        </div>
+      </Router>     
     );
   }
 }
