8bdafd5ae501c6ec3fbb49b0e7d8929c9d6fadf1
Dan Klasson <dan.g.klasson@gmail.com>
2018-12-02 00:11:47 +0700

    tabs as 2 spaces

--- src/Tweets.js
+++ src/Tweets.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import TweetColumn from './TweetColumn'
 
 class Tweets extends Component {
-    render() {
-        const column = "Foo"
-        return (
-            <div>
-                <h2>Tweets Here</h2>
-                <TweetColumn column={ column } />
-            </div>
-        )
-    }
+  render() {
+    const column = "Foo"
+    return (
+      <div>
+        <h2>Tweets Here</h2>
+        <TweetColumn column={column} />
+      </div>
+    )
+  }
 }
 export default Tweets
--- src/reducers/tweetReducer.js
+++ src/reducers/tweetReducer.js
@@ -1,18 +1,18 @@
 import { FETCH_TWEETS } from '../actions/types'
 
 const initialState = {
-    items: [],
-    item: {}
+  items: [],
+  item: {}
 }
 
 export default function(state = initialState, action) {
-    switch(action.type) {
-        case FETCH_TWEETS:
-            return {
-                ...state,
-                items: action.data
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case FETCH_TWEETS:
+      return {
+        ...state,
+        items: action.data
+      };
+    default:
+      return state;
+  }
 }
