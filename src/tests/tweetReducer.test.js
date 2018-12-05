786c1dabfd6bd1c512c956d3eef2d665dbe6cfd9
Dan Klasson <dan.g.klasson@gmail.com>
2018-12-06 04:48:54 +0800

    clean up

--- src/tests/tweetReducer.test.js
+++ src/tests/tweetReducer.test.js
@@ -2,19 +2,23 @@ import tweetReducer from '../reducers/tweetReducer'
 import { FETCH_TWEETS, FETCH_TWEETS_SUCCESS, FETCH_TWEETS_FAIL } from '../actions/types'
 
 describe('Tweet Reducer', () => {
+
   it('should return the initial state', () => {
     expect(tweetReducer(undefined, {type: "unexpected"})).toEqual({
-      items: [],
-      item: {}
+      items: {},
     })
   })
-  
+
   it('should handle FETCH_TWEETS_SUCCESS', () => {
     expect(tweetReducer([], {
       type: FETCH_TWEETS_SUCCESS,
-      data: "Fetch data"
+      data: [{
+        id: 1,
+        user: {screen_name: "Foo"},
+        text: "Fetch data"
+      }]
     })).toEqual({
-      items: "Fetch data"
+      "items": {"Foo": [{"id": 1, "text": "Fetch data", "user": {"screen_name": "Foo"}}]}
     })
   })
 
